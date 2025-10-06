62759f8a3d6af1a6f082e81d77afb5eab80228b4
arctansiago3301 <santiagovelazquez390@gmail.com>
2025-10-06 18:28:55 -0300

    nashe

--- rebalse/main.c
+++ rebalse/main.c
@@ -18,6 +18,20 @@ const char* rightwards_T    = "├";
 const char* horizontal_line = "─";
 const char* vertical_line   = "│";
 
+typedef struct{
+    float max_evo_succ;
+    float med_evo_succ;
+    float max_evo_fail;
+    float avg_evo_fail;
+    float s_evo_succ;
+    float s_evo_fail;
+}costos;
+
+typedef struct {
+    int evos_succ;
+    int evos_fail;
+} cantidades;
+
 void codigoToMayus(char cod[]){
     for(int i = 0; cod[i] != '\0'; i++) {
         cod[i] = toupper((unsigned char) cod[i]);
@@ -43,21 +57,81 @@ void memorizacion(rebalse_separado *rs, int *exito, int *cant){
             if (fgets(aux.condicion, sizeof(aux.condicion), fp) == NULL) break;
             aux.condicion[strcspn(aux.condicion, "\n")] = '\0';
             alta_RS(rs,aux,exito, cant);
-            /*if(*exito != 1) printf("(%d) No se pudo cargar el estudiante\n", iter);
+            if(*exito != 1) printf("(%d) No se pudo cargar el estudiante\n", iter);
             else{
                 printf("(%d) Estudiante cargado. Cant: %d\n",iter, *cant);
                 //mostrarEstudiante(aux);
-            }*/
+            }
             iter++;
         }
         printf("Se cargaron %d estudiantes\n", *cant);
-        fclose(fp);
-        *exito = 1;
+        fclose(fp); 
+        *exito = 1; 
     }else{int h = 0;
         printf("No se encontro el archivo Alumnos.txt\n");
         system("pause");
         *exito = 0;
     }
+}
+
+/*void compararEstructuras(rebalse_separado *rs, int *exito){
+
+}
+*/
+void mostrarRS(rebalse_separado rs){
+    int j, aux;
+    Estudiante eaux;
+    nodo* naux;
+    for(int i=0;i<MAX;i++){
+        if(rs.tabla[i] == NULL){
+            if(i/10 == 0) printf("[ 0%d ]%s%s%s%s%s%s> ", i, horizontal_line,horizontal_line,horizontal_line,horizontal_line,horizontal_line,horizontal_line);
+            else printf("[ %d ]%s%s%s%s%s%s> ", i, horizontal_line, horizontal_line,horizontal_line,horizontal_line,horizontal_line,horizontal_line);
+            printf("[               NULL                 ]\n");
+            //printf("[    ]\n");
+        }
+        else{
+            if(i<10) printf("[ 0%d ]%s%s%s%s%s%s> ", i,horizontal_line, horizontal_line,horizontal_line,horizontal_line, downwards_T,horizontal_line);
+            else printf("[ %d ]%s%s%s%s%s%s> ", i,horizontal_line,horizontal_line,horizontal_line, horizontal_line,downwards_T,horizontal_line);
+            while(1){
+                naux = rs.tabla[i];
+                eaux = naux->e;
+                printf("%s                                    %s\n", upper_margin_left, upper_margin_right);
+                printf("[    ]    %s   %s Codigo: %s                    %s\n",vertical_line, vertical_line,eaux.codigo,vertical_line);
+                //Apellido
+                printf("[    ]    %s   %s Apellido: %s",vertical_line, vertical_line, eaux.apellido);
+                for(j=0; j < (25 - strlen(eaux.apellido)); j++) printf(" ");
+                printf("%s\n", vertical_line);
+                //Nombre
+                printf("[    ]    %s   %s Nombre: %s",vertical_line, vertical_line, eaux.nombre);
+                for(j=0; j < (27 - strlen(eaux.nombre)); j++) printf(" ");
+                printf("%s\n", vertical_line);
+                //Correo
+                printf("[    ]    %s   %s Correo: %s",vertical_line, vertical_line, eaux.correo);
+                for(j=0; j < (27 - strlen(eaux.correo)); j++) printf(" ");
+                printf("%s\n", vertical_line);
+                //Nota
+                printf("[    ]    %s   %s Nota: %.2f",vertical_line, vertical_line, eaux.nota);
+                if(eaux.nota == 10) aux = 24;
+                else aux = 25;
+                for(j=0; j < aux; j++) printf(" ");
+                printf("%s\n", vertical_line);
+                //Condicion
+                printf("[    ]    %s   %s Condicion: %s",vertical_line, vertical_line, eaux.condicion);
+                for(j=0; j < (24 - strlen(eaux.condicion)); j++) printf(" ");
+                printf("%s\n", vertical_line);
+                printf("[    ]    %s   %s                                    %s\n",vertical_line,lower_margin_left, lower_margin_right);
+                printf("[    ]    %s\n", vertical_line);
+                //printf(" %s --> ", t.tabla[i].cur->e.apellido);
+                naux = naux->next;
+                if(naux == NULL){
+                    printf("[    ]    %s%s> [               NULL                 ]\n", lower_margin_left, horizontal_line);
+                    //printf("[    ]\n");
+                    break;
+                } else printf("[    ]    %s%s> ", rightwards_T, horizontal_line);
+            }
+        }
+        //printf("[    ]\n");
+     }
 }
 
 int main(){
@@ -92,6 +166,9 @@ int main(){
         scanf("%d", &opc);
         switch(opc){
             case 3: memorizacion(&rs,&exito,&cant);
+                    break;
+            case 4: mostrarRS(rs);
+                    break;
             case 0: break;
             default:
                 printf("Opcion no valida o funcion en proceso\n");
--- rebalse/rebalse_separado.h
+++ rebalse/rebalse_separado.h
@@ -14,13 +14,15 @@ typedef struct n{
 typedef struct{
     nodo* tabla[M];
     int ult;
+    nodo* cur;
+    nodo* aux;
 }rebalse_separado;
 
 void init_RS(rebalse_separado *r){
     r->ult = -1;
 
     for(int i=0; i<M; i++){
-        r->tabla[i] = r->tabla[i]->next =  NULL;
+        r->tabla[i] =  NULL;
     }
 }
 
@@ -34,26 +36,29 @@ int hash(char code[]){
     return contador%M;
 }
 
-void localizar_RS(rebalse_separado r, char code[], int *exito){
+void localizar_RS(rebalse_separado *r, char code[], int *exito){
     printf("buscando\n");
     int index = hash(code);
-    if(r.tabla[index]->next == NULL){
-        *exito = 0;
-        return; //lista vacia
-    }
-    else{
-        nodo* actual = r.tabla[index]->next;
-        while(actual != NULL){
-            if(strcmp(actual->e.codigo, code) != 0) actual = actual->next;
+    r->cur = r->tabla[index];
+    while(r->cur != NULL){
+        if(strcmp(r->cur->e.codigo, code) == 0) break;
+        else{
+            r->aux = r->cur;
+            r->cur = r->cur->next;
         }
-        if(actual == NULL) *exito = 0; //llego al final de la estructura (no encontrado)
-        else *exito = 1;
+        
+        
     }
+    if(r->cur == NULL) *exito = 0;
+    else *exito = 1;
 }
 
 void alta_RS(rebalse_separado *r, Estudiante e, int *exito, int *cant){
-    localizar_RS(*r,e.codigo,exito);
-    if(*exito) *exito = 0; //repetido
+    localizar_RS(r,e.codigo,exito);
+    if(*exito){
+        *exito = 0; //repetido
+        printf("repetido\n");
+    }   
     else{
         printf("cargando\n");
         //insertar al inicio de la lista
@@ -61,18 +66,28 @@ void alta_RS(rebalse_separado *r, Estudiante e, int *exito, int *cant){
         printf("%p\n", nuevo);
         if(nuevo == NULL){
             *exito = -1;
+            printf("falla de memoria\n");
             return; //falla de memoria
         }
         printf("memoria asignada\n");
         nuevo->e = e;
-        printf("lista.cur = %p", r->tabla[hash(e.codigo)]);
-        //if(r->tabla[hash(e.codigo)]->next == NULL) printf("lista->next = NULL\n");
-        //else printf("lista->next = %p\n", r->tabla[hash(e.codigo)]->next);
-
-        nuevo->next = r->tabla[hash(e.codigo)]->next->next;
-        r->tabla[hash(e.codigo)]->next = nuevo;
+        nuevo->next = r->tabla[hash(e.codigo)];
+        r->tabla[hash(e.codigo)] = nuevo;
         *exito = 1;
-        *cant++;
+        (*cant)++;
+        printf("cargado\n");
+    }
+}
+
+void baja_RS(rebalse_separado *r, Estudiante e, int *exito, int *cant){
+    localizar_RS(r,e.codigo,exito);
+    if(!(*exito)) printf("no encontrado\n");
+    else{
+        if(
+           //comparación de la nupla 
+          ){
+            //baja
+          } 
     }
 }
 

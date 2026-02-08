3df5ab9d7a0bc2a330469b8cf109e8f15ee0d5ae
Santiago3301 <santiagovelazquez390@gmail.com>
2026-02-08 01:27:44 -0300

    Agregué un texto de introducción al programa y a las listas.

--- main.c
+++ main.c
@@ -9,10 +9,15 @@
 int main(){
   int system_clear_option;
   int opc = 1;
+  int intros = 1;
+
+  introduccion_estructuras(system_clear_option);
+  clear(system_clear_option);
 
   printf("Seleccione su tipo de Sistema Operativo\n");
   printf("1. Windows\n2. Linux\nOpcion: ");
   scanf("%d", &system_clear_option);
+  
 
   while(opc != 0){
     clear(system_clear_option);
@@ -20,21 +25,44 @@ int main(){
     printf("Santiago Velazquez - feb. 2026\n");
     printf("Ing. en Computación - UNSL\n\n");
     printf("Seleccine una categoria:\n");
-    printf("1. Listas\n2. Arboles\n3. Rebalses\n0. Salir\nOpcion: ");
+    printf("1. Listas\n2. Arboles\n3. Rebalses\n");
+    if(intros == 1) printf("4. Desactivar textos introductorios\n");
+    else printf("4. Activar textos introductorios\n");
+    printf("0. Salir\nOpcion: ");
     scanf("%d", &opc);
 
     switch(opc){
       case 1:
+          if(intros == 1){
+            introduccion_listas(system_clear_option);
+          }
           menuListas(system_clear_option);
           break;
       case 2:
-          printf("\nud. selecciono arboles\n");
+          if(intros == 1){
+            clear(system_clear_option);
+            printf("intro a arboels");
+          }
+          printf("\nOpcion en proceso\n");
           pause();
           break;
       case 3:
-          printf("\nud. selecciono rebalses\n");
+          if(intros == 1){
+            clear(system_clear_option);
+            printf("intro a rebalses\n");
+          }
+          printf("\nOpcion en proceso\n");
           pause();
           break;
+      case 4:
+          if(intros == 1){
+            intros = 0;
+            printf("\nTextos introductorios desactivados.\n");
+          }else{
+            intros = 1;
+            printf("\nTextos introductorios activados.\n");
+          }
+          pause();
       case 0:
           break;
     }
--- utils.h
+++ utils.h
@@ -13,7 +13,62 @@ static inline void clear(int opc) {
 }
 
 void pause(){
+  printf("\nPresione ENTER para continuar...\n");
   getchar();
   getchar();
 }
+
+void introduccion_estructuras(int clear_option){
+  clear(clear_option);
+  printf("Bienvenido!\n");
+  printf("Este programa contiene implementaciones y ejemplos simples\n");
+  printf("de uso de algunas de las estructuras de datos vistas en la\n");
+  printf("materia Estructuras de Datos y Algoritmos, en la Universi-\n");
+  printf("dad Nacional de San Luis.\n\n");
+  printf("Estas implementaciones fueron realizadas entre los meses de\n");
+  printf("enero y febrero de 2026, mientras me estaba preprando para\n");
+  printf("rendir el final de la materia.\n\n");
+  printf("Cada categoría y estructura tendra una breve introduccion a\n");
+  printf("la misma. En el menu principal las podes desactivar.\n\n");
+  printf("Gracias por usar. Disfrute su visita!\n\n");
+  printf(" -- Santiago Velazquez\n");
+  pause();
+}
+
+void introduccion_listas(int clear_option){
+  clear(clear_option);
+  printf("---- > Listas < ----\n\n");
+  printf("Una lista es la forma más simple e intuitiva de\n");
+  printf("almacenar un conjunto. Consiste de agrupar los\n");
+  printf("elementos uno detrás del otro de manera tal que\n");
+  printf("cada uno tiene un predecesor y un sucesor, ex-\n");
+  printf("cepto por el primer y último elemento, respec-\n");
+  printf("tivamente.\n\n");
+  printf("Hay dos formas de representar una lista:\n\n");
+  printf("- Representacion Secuencial:\n");
+  printf("Almacenamos los elementos en celdas contiguas\n");
+  printf("de memoria, lo que llamamos <arreglo>.\n");
+  printf("En este programa, representamos un arreglo de\n");
+  printf("esta forma:\n\n");
+  printf("[ a | b | c | ... | z ]\n\n");
+  printf("Si no se conoce el largo de la lista, la gra-\n");
+  printf("ficamos asi:\n\n");
+  printf("[ a | b | ... | x | y | * ]\n\n");
+  printf("Donde <*> es la marca de fin de la lista.\n\n");
+  printf("- Representacion Vinculada:\n");
+  printf("Almacenamos los elementos en direcciones de\n");
+  printf("memoria no contiguas, en celdas que llamamos\n");
+  printf("<Nodos>. Cada nodo contiene un campo <valor>\n");
+  printf("y un apuntador a otro nodo.\n");
+  printf("Aquí los representamos de esta forma:\n\n");
+  printf("[ valor |o]-->\n\n");
+  printf("Una lista vinculada tiene esta forma:\n\n");
+  printf("o--> [ a |o]--> [ b |o]--> ... [ z |o]--> Ii.\n\n");
+  printf("El simbolo <Ii.> pretende imitar el simbolo de\n");
+  printf("tierra electrica. Representa <null>, una di-\n");
+  printf("rección de memoria no valida para un nodo.\n");
+  printf("La usamos como marca de fin para la lista vinculada.\n");
+  pause();
+}
+
 #endif
